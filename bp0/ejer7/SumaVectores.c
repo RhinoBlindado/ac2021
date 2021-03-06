e6ed23c186f69810630b5c7c9c100494f7a0220f
Valentino Lugli <valentinolugli@gmail.com>
2021-03-06 23:01:46 +0100

    [bp0] 75% completed
    Changes
    	- Completed up to the exercise 6
    	- Folder for exercise 7

--- bp0/ejer7/SumaVectores.c
+++ bp0/ejer7/SumaVectores.c
@@ -10,19 +10,19 @@
 */ 
 
 #include <stdlib.h>	// biblioteca con funciones atoi(), malloc() y free()
-#include <stdio.h>	// biblioteca donde se encuentra la funciÃ³n printf()
-#include <time.h>	// biblioteca donde se encuentra la funciÃ³n clock_gettime() 
+#include <stdio.h>	// biblioteca donde se encuentra la función printf()
+#include <time.h>	// biblioteca donde se encuentra la función clock_gettime() 
 
-//SÃ³lo puede estar definida una de las tres constantes VECTOR_ (sÃ³lo uno de los ... 
+//Solo puede estar definida una de las tres constantes VECTOR_ (Solo uno de los ... 
 //tres defines siguientes puede estar descomentado):
-//#define VECTOR_LOCAL	// descomentar para que los vectores sean variables ...
+#define VECTOR_LOCAL	// descomentar para que los vectores sean variables ...
 			// locales (si se supera el tamaÃ±o de la pila se ...
-			// generarÃ¡ el error "ViolaciÃ³n de Segmento")
+			// generarÃ¡ el error "Violación de Segmento")
 //#define VECTOR_GLOBAL // descomentar para que los vectores sean variables ...
 			// globales (su longitud no estarÃ¡ limitada por el ...
 			// tamaÃ±o de la pila del programa)
-#define VECTOR_DYNAMIC	// descomentar para que los vectores sean variables ...
-			// dinÃ¡micas (memoria reutilizable durante la ejecuciÃ³n)
+//#define VECTOR_DYNAMIC	// descomentar para que los vectores sean variables ...
+			// dinÃ¡micas (memoria reutilizable durante la ejecución)
 
 #ifdef VECTOR_GLOBAL
 #define MAX 33554432	//=2^25
@@ -34,18 +34,18 @@ int main(int argc, char** argv){
   
   int i; 
 
-  struct timespec cgt1,cgt2; double ncgt; //para tiempo de ejecuciÃ³n
+  struct timespec cgt1,cgt2; double ncgt; //para tiempo de ejecución
 
-  //Leer argumento de entrada (nÂº de componentes del vector)
+  //Leer argumento de entrada (nº de componentes del vector)
   if (argc<2){	
-    printf("Faltan nÂº componentes del vector\n");
+    printf("Faltan nº componentes del vector\n");
     exit(-1);
   }
   
-  unsigned int N = atoi(argv[1]);	// MÃ¡ximo N =2^32-1=4294967295 (sizeof(unsigned int) = 4 B)
+  unsigned int N = atoi(argv[1]);	// Máximo N =2^32-1=4294967295 (sizeof(unsigned int) = 4 B)
   //printf("TamaÃ±o Vectores:%u (%u B)\n",N, sizeof(unsigned int)); 
   #ifdef VECTOR_LOCAL
-  double v1[N], v2[N], v3[N];   // TamaÃ±o variable local en tiempo de ejecuciÃ³n ...
+  double v1[N], v2[N], v3[N];   // TamaÃ±o variable local en tiempo de ejecución ...
 				// disponible en C a partir de C99 
   #endif
   #ifdef VECTOR_GLOBAL
@@ -53,7 +53,7 @@ int main(int argc, char** argv){
   #endif
   #ifdef VECTOR_DYNAMIC
   double *v1, *v2, *v3;
-  v1 = (double*) malloc(N*sizeof(double));// malloc necesita el tamaÃ±o en bytes
+  v1 = (double*) malloc(N*sizeof(double));// malloc necesita el tamaño en bytes
   v2 = (double*) malloc(N*sizeof(double));
   v3 = (double*) malloc(N*sizeof(double));
   if ((v1 == NULL) || (v2 == NULL) || (v2 == NULL)) {	
@@ -88,15 +88,15 @@ int main(int argc, char** argv){
   ncgt=(double) (cgt2.tv_sec-cgt1.tv_sec)+
        (double) ((cgt2.tv_nsec-cgt1.tv_nsec)/(1.e+9));
 
-  //Imprimir resultado de la suma y el tiempo de ejecuciÃ³n
+  //Imprimir resultado de la suma y el tiempo de ejecución
   if (N<10) {
-  printf("Tiempo:%11.9f\t / TamaÃ±o Vectores:%u\n",ncgt,N); 
+  printf("Tiempo:%11.9f\t / Tamaño Vectores:%u\n",ncgt,N); 
   for(i=0; i<N; i++) 
     printf("/ V1[%d]+V2[%d]=V3[%d](%8.6f+%8.6f=%8.6f) /\n",
            i,i,i,v1[i],v2[i],v3[i]); 
   }
   else
-    printf("Tiempo:%11.9f\t / TamaÃ±o Vectores:%u\t/ V1[0]+V2[0]=V3[0](%8.6f+%8.6f=%8.6f) / / V1[%d]+V2[%d]=V3[%d](%8.6f+%8.6f=%8.6f) /\n",
+    printf("Tiempo:%11.9f\t / Tamaño Vectores:%u\t/ V1[0]+V2[0]=V3[0](%8.6f+%8.6f=%8.6f) / / V1[%d]+V2[%d]=V3[%d](%8.6f+%8.6f=%8.6f) /\n",
            ncgt,N,v1[0],v2[0],v3[0],N-1,N-1,N-1,v1[N-1],v2[N-1],v3[N-1]); 
  
 
